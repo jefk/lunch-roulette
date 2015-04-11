c962fca6e47f5d16ff1b973fa0aa9e71e3572f45
jefk <speedracer111@gmail.com>
2015-04-11 02:02:40 -0400

    add tags

--- db/migrate/20150411055939_create_tags.rb
+++ db/migrate/20150411055939_create_tags.rb
@@ -1,6 +1,6 @@
-class CreateItems < ActiveRecord::Migration
+class CreateTags < ActiveRecord::Migration
   def change
-    create_table :items do |t|
+    create_table :tags do |t|
       t.string :name, index: true
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407222420) do
+ActiveRecord::Schema.define(version: 20150411055939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,5 +21,15 @@ ActiveRecord::Schema.define(version: 20150407222420) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "items", ["name"], name: "index_items_on_name", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
 
 end
